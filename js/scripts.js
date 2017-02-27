3bf03e6f656a4cdfa8593a7f82ee21a03f74b3fa
Janek Brandt and Mark Fisher <mark@>
2017-02-27 08:57:23 -0800

    add basic form for user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@
 
 // Front End
 $(function(){
+  $("form#destinationForm").submit(function() {
+    event.preventDefault();
+    var inputLocation = $("input#location").val();
 
+    var destination = new Destination(inputLocation);
+    console.log(destination);
+
+  })
 });
