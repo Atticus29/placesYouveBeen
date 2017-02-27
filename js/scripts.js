826190137fab341f9862941d8fc7c439c23172ab
Janek Brandt and Mark Fisher <mark@>
2017-02-27 09:53:45 -0800

    add div for new landmarks in html, add function to add more of these divs, and add loop over divs on submit to add each as separate Landmark object to landmarks array in Destination object

--- js/scripts.js
+++ js/scripts.js
@@ -18,20 +18,43 @@ Destination.prototype.addLandmark = function(name, notes){
 
 // Front End
 $(function(){
+
+  function addLandmarkDiv() {
+    $("#new-landmarks").append('<div class="new-landmark">' +
+      '<div class="form-group">' +
+        '<label for="landmark">Enter a Landmark:</label>' +
+        '<input type="text" class="form-control landmark-input">' +
+        '<label for="notes">Enter notes about Landmark:</label>' +
+        '<input type="text" class="form-control notes-input">' +
+      '</div>' +
+    '</div>');
+  };
+
+  $("#landmarkButton").click(function() {
+    addLandmarkDiv();
+  });
+
+
+
   $("form#destinationForm").submit(function() {
     event.preventDefault();
     var inputLocation = $("input#location").val();
     var inputTimeOfYear = $("input#timeOfYear").val();
-    var inputLandmark = $("input#landmark").val();
-    var inputNotes = $("input#notes").val();
 
     var destination1 = new Destination(inputLocation, inputTimeOfYear);
-    destination1.addLandmark(inputLandmark, inputNotes);
+    $(".new-landmark").each(function(){
+      var inputLandmark = $(this).find(".landmark-input").val();
+      var inputNotes = $(this).find(".notes-input").val();
+      destination1.addLandmark(inputLandmark, inputNotes);
+    });
     console.log(destination1);
     var inputLocation = $("input#location").val("");
     var inputTimeOfYear = $("input#timeOfYear").val("");
     var inputLandmark = $("input#landmark").val("");
     var inputNotes = $("input#notes").val("");
+
+    $("#new-landmarks").empty();
+    addLandmarkDiv();
 
   })
 });
