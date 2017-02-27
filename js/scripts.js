b1ad22b1890c42e225ac43252f2f10ec4b5da0db
Janek Brandt and Mark Fisher <mark@>
2017-02-27 09:07:59 -0800

    add Destination object constructor and addLandmark method for Destination

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 // Back End
 
+function Destination (location, timeOfYear, notes){
+  this.location = location;
+  this.landmarks = [];
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+Destination.prototype.addLandmark = function(landmark){
+  this.landmarks.push(landmark);
+}
+
 // Front End
 $(function(){
   $("form#destinationForm").submit(function() {
     event.preventDefault();
     var inputLocation = $("input#location").val();
 
-    var destination = new Destination(inputLocation);
-    console.log(destination);
+    var destination1 = new Destination(inputLocation);
+    console.log(destination1);
 
   })
 });
