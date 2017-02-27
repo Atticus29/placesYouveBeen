4f167edd70fe3e43528ad61aaa89c7d22b2d88e1
Janek Brandt and Mark Fisher <janek@>
2017-02-27 09:29:09 -0800

    add Landmark object constructor and change addLandmark method to push new landmark object to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 // Back End
 
-function Destination (location, timeOfYear, notes){
+function Destination (location, timeOfYear){
   this.location = location;
   this.landmarks = [];
   this.timeOfYear = timeOfYear;
+}
+
+function Landmark(name, notes) {
+  this.name = name;
   this.notes = notes;
 }
 
-Destination.prototype.addLandmark = function(landmark){
-  this.landmarks.push(landmark);
+Destination.prototype.addLandmark = function(name, notes){
+  var newLandmark = new Landmark(name, notes);
+  this.landmarks.push(newLandmark);
 }
 
 // Front End
@@ -16,9 +21,17 @@ $(function(){
   $("form#destinationForm").submit(function() {
     event.preventDefault();
     var inputLocation = $("input#location").val();
+    var inputTimeOfYear = $("input#timeOfYear").val();
+    var inputLandmark = $("input#landmark").val();
+    var inputNotes = $("input#notes").val();
 
-    var destination1 = new Destination(inputLocation);
+    var destination1 = new Destination(inputLocation, inputTimeOfYear);
+    destination1.addLandmark(inputLandmark, inputNotes);
     console.log(destination1);
+    var inputLocation = $("input#location").val("");
+    var inputTimeOfYear = $("input#timeOfYear").val("");
+    var inputLandmark = $("input#landmark").val("");
+    var inputNotes = $("input#notes").val("");
 
   })
 });
